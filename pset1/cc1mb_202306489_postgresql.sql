404b8db33b7fb674bb0748c2d0729dd1fe2550d8
Joao Victor da Silva Cunha <124682784+J0aoCunha@users.noreply.github.com>
2023-05-19 18:36:47 -0300

    Update cc1mb_202306489_postgresql.sql

--- pset1/cc1mb_202306489_postgresql.sql
+++ pset1/cc1mb_202306489_postgresql.sql
@@ -1,5 +1,13 @@
-CREATE USER  JOAOVICTORDASILVACUNHA
-DROP 
+DROP DATABASE IF EXISTS UVV;
+CREATE USER  JOAO
+CREATE DATABASE UVV 
+OWNER  JOAO;
+TEMPLATE template0;
+ENCODING UTF8;
+LC_COLLATE  "pt_BR.UTF-8";
+LC_CTYPE "pt_BR.UTF-8";
+ALLOW_CONNECTIONS  true;
+CREATE SCHEMA lojas;
 CREATE TABLE produtos (
                 produto_id                     NUMERIC(38)     NOT NULL,
                 nome                           VARCHAR(255)    NOT NULL,
